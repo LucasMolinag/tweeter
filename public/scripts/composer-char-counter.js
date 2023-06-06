d83e7f61346b9f39304c7f449380e0fff94f02aa
Lucas <molinag.lucas@gmail.com>
2023-06-06 14:22:48 -0700

    Char counter update fix - Textarea wrap fix

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,25 @@
 /*
 Count number of characters available to be used to write the tweet - down from 140
 */
+
+function setCharactersCounter(textarea) {
+  let textareaValue = textarea.val();
+  let charactersLeft = 140 - textareaValue.length;
+
+  let counterElement = textarea.closest('.new-tweet').find(".counter");
+
+  counterElement.text(charactersLeft);
+
+  //Invalidation conditional
+  if (charactersLeft < 0) {
+    counterElement.addClass('invalid');
+  } else {
+    counterElement.removeClass('invalid');
+  }
+}
+
 $(document).ready(function() {
   $(".new-tweet textarea").on("input", function() {
-    let textareaValue = $(this).val();
-    let charactersLeft = 140 - textareaValue.length;
-    console.log(charactersLeft);
-
-    let counterElement = $(this).closest('.new-tweet').find(".counter");
-
-    counterElement.text(charactersLeft);
-//Invalidation conditional
-    if (charactersLeft < 0) {
-      counterElement.addClass('invalid');
-    } else {
-      counterElement.removeClass('invalid');
-    }
+    setCharactersCounter($(this));
   });
 });
